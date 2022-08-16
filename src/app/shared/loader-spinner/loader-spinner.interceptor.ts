5e8b36f4198385f935b53dcae7ad49b5dd7a78de
唯有機小幫手 <53168882+a0988591269@users.noreply.github.com>
2022-08-16 17:32:23 +0800

    update loader-spinner

--- src/app/shared/loader-spinner/loader-spinner.interceptor.ts
+++ src/app/shared/loader-spinner/loader-spinner.interceptor.ts
@@ -11,6 +11,9 @@ import { finalize } from 'rxjs/operators';
 
 @Injectable()
 export class LoaderSpinnerInterceptor implements HttpInterceptor {
+  totalRequests = 0;
+  requestsCompleted = 0;
+
   constructor(private loaderService: LoaderSpinnerService) {}
 
   intercept(
@@ -18,6 +21,18 @@ export class LoaderSpinnerInterceptor implements HttpInterceptor {
     next: HttpHandler
   ): Observable<HttpEvent<unknown>> {
     this.loaderService.show();
-    return next.handle(request).pipe(finalize(() => this.loaderService.hide()));
+    this.totalRequests++;
+
+    return next.handle(request).pipe(
+      finalize(() => {
+        this.requestsCompleted++;
+
+        if (this.requestsCompleted === this.totalRequests) {
+          this.loaderService.hide();
+          this.totalRequests = 0;
+          this.requestsCompleted = 0;
+        }
+      })
+    );
   }
 }
